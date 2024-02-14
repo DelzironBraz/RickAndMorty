dad81a40ba770af6718e25fa6a90cb0396ac325b
Delziron Lima <delziron.braz@hotmail.com>
2024-02-13 22:15:05 -0300

    feat: add locations page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import CharacterFilter from "../components/CharacterFIlter";
+import CharacterFilter from "../components/CharacterFilter";
 import CharacterGrid from "../components/CharacterGrid";
 import Banner from "../components/Banner";
 import banner from '../assets/banner_home.png'
@@ -9,7 +9,7 @@ const Home = () => {
     return (
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', alignContent: 'center', flexDirection: 'column', marginTop: '4rem' }}>
             <Box sx={{ width: '80%' }}>
-                <Banner image={banner} />
+                <Banner image={banner} width={600} height={200} />
                 <CharacterFilter />
                 <CharacterGrid />
             </Box>
